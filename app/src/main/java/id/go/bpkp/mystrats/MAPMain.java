30342d75f4a94be7c301b8f03b9fa915e33ec21f
Septian Adi Nugraha <septian.adi.nugraha@gmail.com>
2018-08-14 11:06:25 +0700

    Initial Commit

---
+++ app/src/main/java/id/go/bpkp/mystrats/MAPMain.java
@@ -0,0 +1,13 @@
+package id.go.bpkp.mystrats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MAPMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapmain);
+    }
+}
