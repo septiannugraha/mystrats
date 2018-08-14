9efdfd2874ef3bde147a759078245c2d4d16e749
Pheromone89 <mas.muhamad.dzulfikar@gmail.com>
2018-08-14 15:01:40 +0700

    commit di fragment home

--- app/src/main/java/id/go/bpkp/mystrats/view/HomeFragment.java
+++ app/src/main/java/id/go/bpkp/mystrats/view/HomeFragment.java
@@ -2,10 +2,14 @@ package id.go.bpkp.mystrats.view;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import id.go.bpkp.mystrats.R;
 
@@ -13,6 +17,8 @@ import id.go.bpkp.mystrats.R;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    CardView dataPegawaiButton, dataDiklatButton;
 
 
     public HomeFragment() {
@@ -27,4 +33,24 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dataPegawaiButton = view.findViewById(R.id.data_pegawai_button);
+        dataDiklatButton = view.findViewById(R.id.data_diklat_button);
+
+        dataPegawaiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "data pegawai", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dataDiklatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "data diklat", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
